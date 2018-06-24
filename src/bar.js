835400a1d70d9ce48ec53208c6d6d2c2cf117b70
Kim Dalsgaard <kim@kimdalsgaard.com>
2018-06-24 12:44:39 +0200

    Using baz and utils

--- src/bar.js
+++ src/bar.js
@@ -1,11 +1,21 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element';
+import { sum } from '@jyskebank/jb-utils/sum';
+import '@jyskebank/jb-baz'
 
 export class Bar extends PolymerElement {
   
   static get template() {
     return html`
-      <section>Bar!</section>
+      <section>
+        <h5>Bar {{bar}}</h5>
+        <jb-baz></jb-baz>
+      </section>
     `;
+  }
+
+  ready () {
+    super.ready();
+    this.bar = sum(29, 13);
   }
 
 }
